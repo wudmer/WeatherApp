2ed5f5d0ae080a8d009ef1a5b3fbebb557b4af63
Wudmer <Abaza.org@gmail.com>
2017-12-23 23:26:04 +0200

    refactored code in ForecastListAdapter, improved imports

--- app/src/main/java/org/abaza/weatherapp/ui/adapter/ForecastListAdapter.kt
+++ app/src/main/java/org/abaza/weatherapp/ui/adapter/ForecastListAdapter.kt
@@ -4,14 +4,12 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_forecast.view.*
 import org.abaza.weatherapp.R
 import org.abaza.weatherapp.domain.model.Forecast
 import org.abaza.weatherapp.domain.model.ForecastList
 import org.abaza.weatherapp.ui.utils.ctx
-import org.jetbrains.anko.find
 
 class ForecastListAdapter(private val weekForecast: ForecastList,
                           val itemClick: (Forecast)-> Unit) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
@@ -28,19 +26,14 @@ class ForecastListAdapter(private val weekForecast: ForecastList,
     override fun getItemCount(): Int = weekForecast.size
 
     class ViewHolder(view: View, val itemClick: (Forecast)-> Unit) : RecyclerView.ViewHolder(view) {
-        private val iconView = view.find<ImageView>(R.id.icon)
-        private val dateView = view.find<TextView>(R.id.date)
-        private val descriptionView = view.find<TextView>(R.id.description)
-        private val maxTemperatureView = view.find<TextView>(R.id.maxTemperature)
-        private val minTemperatureView = view.find<TextView>(R.id.minTemperature)
 
         fun bindForecast(forecast: Forecast) {
             with(forecast) {
-                Picasso.with(itemView.ctx).load(iconUrl).into(iconView)
-                dateView.text = date
-                descriptionView.text = description
-                maxTemperatureView.text = "${high}º"
-                minTemperatureView.text = "${low}º"
+                Picasso.with(itemView.ctx).load(iconUrl).into(itemView.icon)
+                itemView.date.text = date
+                itemView.description.text = description
+                itemView. maxTemperature.text = "${high}º"
+                itemView.minTemperature.text = "${low}º"
                 itemView.setOnClickListener { itemClick(this) }
             }
         }
